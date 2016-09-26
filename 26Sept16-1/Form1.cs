6e2406f3b8671973c511b3c63d9eb60b4e593dd6
mmendez9 <hikari1995>
2016-09-26 10:12:05 -0400

    Valid input for Exam 1

--- 26Sept16-1/Form1.cs
+++ 26Sept16-1/Form1.cs
@@ -16,5 +16,37 @@ namespace _26Sept16_1
         {
             InitializeComponent();
         }
+
+        private void btmCalculate_Click(object sender, EventArgs e)
+        {
+            if (isInputValid() == true)
+            {
+                MessageBox.Show("Got valid input", "Average", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool isInputValid()
+        {
+            double exam1, exam2;
+            if (double.TryParse(tbxExam1.Text, out exam1))
+            {
+                if (exam1 >= 0 && exam1 <= 100) 
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Exam 1 out of range", "Average", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Exam 1 missing or not numeric", "Average", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+
+                return false;
+            }
+        }
     }
 }
